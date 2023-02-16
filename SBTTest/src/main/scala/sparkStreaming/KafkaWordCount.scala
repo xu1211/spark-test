ead19c381820468b168556e9ec1957e4b149913b
xu1211 <xu.yuchun@foxmail.com>
2023-02-16 10:20:02 +0800

    feat( #0): kafkaStreaming

---
+++ SBTTest/src/main/scala/sparkStreaming/KafkaWordCount.scala
@@ -0,0 +1,46 @@
+package sparkStreaming
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
+
+object KafkaWordCount {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(10))
+
+    //设置kafka参数，定义brokers集合
+    val brokers = "9.135.89.169:9092"
+    val groupId = "webGroup"
+    val topics = "topic1"
+
+    val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
+      ConsumerConfig.GROUP_ID_CONFIG -> groupId,
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer])
+
+    // 创建流接收消息
+    val messages = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](topicsSet, kafkaParams))
+
+    print("---------:" +messages)
+
+
+    // Get the lines, split them into words, count the words and print
+    val lines = messages.map(_.value)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
+    wordCounts.print()
+
+    // Start the computation
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
